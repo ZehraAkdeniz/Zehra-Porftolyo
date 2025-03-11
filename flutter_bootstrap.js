32fa3de4c4f8c2ba26cfe0a8976866b4f2b240e9
zehraakdeniz <zehranurakdeniz3@gmail.com>
2025-03-11 18:25:18 +0300

    merged

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3145828095"
+    serviceWorkerVersion: "2949159713"
   }
 });
